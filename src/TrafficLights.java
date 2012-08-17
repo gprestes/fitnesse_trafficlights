923b6a0d79144f8a69d7ac0785015dfaec418d5a
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2012-08-17 13:26:38 -0300

    now we have dummy methods. No biggy, but getting there

--- src/TrafficLights.java
+++ src/TrafficLights.java
@@ -1,5 +1,10 @@
 public class TrafficLights {
-//  public static void main(String[] args) {
-//    System.out.println("Hello, World");
-//  }
+
+  public void setPreviousState(String state) {
+
+  }
+
+  public String nextState() {
+    return "red, yellow";
+  }
 }
