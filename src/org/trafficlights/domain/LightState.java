48e45b04e4368506f6d824dec50174aa140af951
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2012-08-20 17:52:38 -0300

    new junit tests

---
+++ src/org/trafficlights/domain/LightState.java
@@ -0,0 +1,16 @@
+package org.trafficlights.domain;
+
+public enum LightState {
+    RED {
+        public LightState next() { return RED_YELLOW; }
+    },
+    RED_YELLOW {
+        public LightState next() { return GREEN; }
+    },
+    GREEN {
+        public LightState next() { return null; }
+    };
+
+    public abstract LightState next();
+
+}
---
+++ src/org/trafficlights/test/acceptance/TrafficLights.java
@@ -0,0 +1,16 @@
+package org.trafficlights.test.acceptance;
+
+public class TrafficLights {
+
+  private String state;
+
+  public void setPreviousState(String state) {
+    this.state = state;
+  }
+
+  public String nextState() {
+    if ("red".equals(state)) 
+      return "red, yellow";
+    return "green";
+  }
+}
