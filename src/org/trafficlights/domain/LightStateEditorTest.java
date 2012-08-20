a1e93386e5da2eb7e09219ee45e6f67c3fc35109
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2012-08-20 20:55:37 -0300

    new tests to convert properties to String (Fitnesse only understand strings)

---
+++ src/org/trafficlights/domain/LightStateEditorTest.java
@@ -0,0 +1,45 @@
+package org.trafficlights.domain;
+
+import static java.util.Arrays.*;
+import static org.junit.Assert.*;
+import static org.trafficlights.domain.LightState.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class LightStateEditorTest {
+
+    @Parameters
+    public static List<Object[]> data() {
+        return asList(new Object[][] {
+            { "red", RED },
+            { "red, yellow", RED_YELLOW }
+        });
+    }
+
+    private LightStateEditor editor  = new LightStateEditor();
+    private String stateName;
+    private LightState state;
+
+    public LightStateEditorTest(String stateName, LightState state) {
+        this.stateName = stateName;
+        this.state = state;
+    }
+
+    @Test
+    public void setAsText() {
+        editor.setAsText(stateName);
+        assertEquals(state, editor.getValue());
+    }
+
+    @Test
+    public void setRed() {
+        editor.setAsText("red");
+        assertEquals(LightState.RED, editor.getValue());
+    }
+}
