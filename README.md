915a494832f0cca415f291f4440c5fe7feafc328
Guilherme Heuser Prestes <guilherme.prestes@gmail.com>
2012-08-26 22:38:21 -0300

    reformatted README. Added if in LightStateEditor for different states comparison.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Steps to execute:
-1- java -DCM_SYSTEM=fitnesse.wiki.cmSystems.GitCmSystem -jar fitnesse.jar -p 8080
-2- Access http://localhost:8080
+
+# Execute the following command "java -jar fitnesse.jar -p 8080"
+# Access http://localhost:8080
 
 If you are using port 8080 for something else you can change the parameter in
 step #1.
--- src/org/trafficlights/domain/LightStateEditor.java
+++ src/org/trafficlights/domain/LightStateEditor.java
@@ -5,6 +5,14 @@ import java.beans.PropertyEditorSupport;
 public class LightStateEditor extends PropertyEditorSupport {
 
     public void setAsText(String state) {
-        setValue(LightState.RED);
+        if ("red".equals(state)) {
+            setValue(LightState.RED);
+            return;
+        }
+        if ("red, yellow".equals(state)) {
+            setValue(LightState.RED_YELLOW);
+            return;
+        }
+        setValue(LightState.GREEN);
     }
 }
