3e89a07a33808bcd127bd7089d4409e4936d7ce4
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2012-09-10 13:53:42 -0300

    added CrossingControl suite

---
+++ src/org/trafficlights/test/acceptance/FirstLightSwitchingCrossingController.java
@@ -0,0 +1,43 @@
+package org.trafficlights.test.acceptance;
+
+import org.trafficlights.domain.LightState;
+
+public class FirstLightSwitchingCrossingController {
+
+    LightState firstState;
+
+    LightState secondState;
+
+    public void setFirstLight(LightState state) {
+        firstState = state;
+    }
+
+    public void setSecondLight(LightState state) {
+        secondState = state;
+    }
+
+    public void execute() {
+        if (!isValidLightStateConfiguration()) {
+            warningConfiguration();
+            return;
+        }
+        firstState = firstState.next();
+    }
+
+    private boolean isValidLightStateConfiguration() {
+        return  !LightState.UNKNOWN.equals(firstState) && LightState.RED.equals(secondState);
+    }
+
+    private void warningConfiguration() {
+        firstState = LightState.UNKNOWN;
+        secondState = LightState.UNKNOWN;
+    }
+
+    public LightState firstLight() {
+        return firstState;
+    }
+
+    public LightState secondLight() {
+        return secondState;
+    }
+}
